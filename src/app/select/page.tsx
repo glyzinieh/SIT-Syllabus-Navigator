e4a4084be25d92e641b0061e44dc3abff4d2f4c3
Haruhiko Fuji <glyzinie.h@gmail.com>
2025-08-20 18:03:38 +0900

    レイアウトを調整

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -2,21 +2,27 @@ import { cookies } from "next/headers";
 import { SelectForm } from "./_components/select";
 
 export default async function Page() {
-  const syllabusIndex = await fetch(
-    "https://glyzinieh.github.io/SIT-Syllabus-Data/index.json"
-  ).then((res) => res.json());
+	const response = await fetch(
+		"https://glyzinieh.github.io/SIT-Syllabus-Data/index.json"
+	);
+	if (!response.ok) {
+		throw new Error("Failed to fetch syllabus");
+	}
+	const syllabusIndex = await response.json();
 
-  const cookieStore = await cookies();
-  const admissionYear = cookieStore.get("admissionYear");
-  const departmentCode = cookieStore.get("departmentCode");
+	const cookieStore = await cookies();
+	const admissionYear = cookieStore.get("admissionYear");
+	const departmentCode = cookieStore.get("departmentCode");
 
-  return (
-    <SelectForm
-      syllabusIndex={syllabusIndex}
-      defaultValues={{
-        admissionYear: admissionYear ? Number(admissionYear.value) : null,
-        departmentCode: departmentCode ? departmentCode.value : null,
-      }}
-    />
-  );
+	return (
+		<SelectForm
+			syllabusIndex={syllabusIndex}
+			defaultValues={{
+				admissionYear: admissionYear
+					? Number(admissionYear.value)
+					: null,
+				departmentCode: departmentCode ? departmentCode.value : null,
+			}}
+		/>
+	);
 }
