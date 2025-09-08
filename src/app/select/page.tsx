4ec81fa321fb37b4d2dd203adaf79aa9e1ffa5a6
Haruhiko Fuji <glyzinie.h@gmail.com>
2025-09-08 22:38:07 +0900

    ページコンポーネントにデータ追加リンクを追加し、シラバス選択のUIを改善

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { SelectForm } from "./_components/select";
+import { Link } from "@/components/digital-go-jp";
 
 export default async function Page() {
 	const response = await fetch(
@@ -15,14 +16,27 @@ export default async function Page() {
 	const departmentCode = cookieStore.get("departmentCode");
 
 	return (
-		<SelectForm
-			syllabusIndex={syllabusIndex}
-			defaultValues={{
-				admissionYear: admissionYear
-					? Number(admissionYear.value)
-					: null,
-				departmentCode: departmentCode ? departmentCode.value : null,
-			}}
-		/>
+		<>
+			<div className="flex items-end gap-4 mb-4">
+				<h1 className="text-3xl font-bold">シラバスを選択する</h1>
+				<Link
+					href="https://github.com/glyzinieh/SIT-Syllabus-Data/blob/main/%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8B.md"
+					target="_blank"
+				>
+					データを追加する
+				</Link>
+			</div>
+			<SelectForm
+				syllabusIndex={syllabusIndex}
+				defaultValues={{
+					admissionYear: admissionYear
+						? Number(admissionYear.value)
+						: null,
+					departmentCode: departmentCode
+						? departmentCode.value
+						: null,
+				}}
+			/>
+		</>
 	);
 }
